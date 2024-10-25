9268d687658db58663f8f35da15d3ace8ebfa22d
josduaper <josduaper@alu.edu.gva.es>
2024-10-25 12:06:26 +0200

    MinimoArray

--- src/main/MarcoCadena.java
+++ src/main/MarcoCadena.java
@@ -1,16 +1,63 @@
-//public class MarcoCadena {
-  //  public static int minimoArray() {
-//        String[] palabros = {"Se vende", "Farola", "Nueva", "O", "Usada"};
-//        String cuadro = "";
-//
-//        for (int i = 0 ; i < palabros.length ; i++) {
-//
+import java.util.Scanner;
+
+public class MarcoCadena {
+    public static int palabraMasLarga(String[] palabros) {
+        int max = 0;
+
+        for (int i = 0 ; i < palabros.length ; i++) {
+
+            if (palabros[i].length() > max) {
+                max = palabros[i].length();
+            }
+
+        }
+        return max;
+    }
+
+    public static String asteriscos(int max) {
+        String asteriscos = "";
+
+        for (int i = 0 ; i <= max + 3 ; i++) {
+
+                asteriscos += "*";
+
+        }
+        return asteriscos;
+
+    }
+
+    public static String cuadro(int max, String[] palabros, String asteriscos) {
+        String cuadro = asteriscos;
+
+        for (int i = 0 ; i < palabros.length ; i++) {
+            cuadro += "\n";
+            cuadro = cuadro + "* " + palabros[i] + (palabros[i].length());
+        }
+
+        cuadro = cuadro + "\n" + asteriscos;
+        return cuadro;
+
+    }
+
+    public static void main(String[] args) {
+        String[] palabros = new String[5];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce 5 cadenas de texto:");
+
+        for (int i = 0; i < palabros.length; i++){
+            palabros[i] = sc.next();
+        }
+
+        int max = palabraMasLarga(palabros);
+        String asteriscos = asteriscos(max);
+
+        System.out.println(cuadro(max, palabros, asteriscos));
 
 
-//        }
-//    }
 
-//    public static void main(String[] args) {
+        sc.close();
+    }
+}
 
-//    }
-//}
+// Por acabar
